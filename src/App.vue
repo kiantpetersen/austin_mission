c964a3fcb66dc4b3559c6ab526e507af1c4d9e9c
unknown <kiantpetersen@gmail.com>
2023-10-23 16:48:18 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+
+
+<template>
+  <div class="game">
+
+    <h1>Ninja reaction Timer</h1><br>
+    <button :disabled="isPlaying" @click="start">Play</button><br>
+    
+    <Block @end="endGame" v-if="isPlaying" :delay="this.delay"/>
+    <Results v-if="score" :score="this.score"/>
+    
+  </div>
+
+</template>
+<script >
+import Results from './components/Results.vue'
+import Block from './components/Block.vue'
+
+export default{
+  name:'App',
+  components:{Block,Results},
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score:null
+    }
+  },
+  methods:{
+    start(){
+      this.delay=2000+Math.random()*5000
+      this.isPlaying=true;
+      this.score=null;
+    }
+    ,endGame(reactionTime){
+      this.score=reactionTime;
+      this.isPlaying=false;
+    }
+  }
+}
+
+</script>
+<style scoped>
+.game{
+  display:flex;
+  flex-direction:column;
+  margin:auto auto;
+  padding:50px;
+  width:100%;
+  margin:0 auto;
+  align-items:center;
+}
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,39 @@
+<template>
+    <p>Reaction time: {{ score }}ms</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+data(){
+    return{
+        rank:null
+    }
+},mounted(){
+    if (this.score<250){
+        this.rank='Ninja fingers'
+    }else if ( this.score<300){
+        this.rank='Rapid Reflexes'
+        
+    }else{
+        this.rank='Snail Pace...'
+    }
+}
+}
+</script>
+
+<style>
+.rank{
+    color:#fff;
+    width:200px;
+    height:50px;
+    display:flex;
+    align-items:center;
+    justify-content: center;
+    border-radius:10px;
+    background: #344eff;
+
+}
+
+</style>
